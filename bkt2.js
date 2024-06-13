b0c00dd0aa4ea391eddb5e5ca88a5d0d0c717963
Đinh Khắc Huy <khachuyd7@gmail.com>
2024-06-13 22:21:13 +0700

    dfdf

---
+++ bkt2.js
@@ -0,0 +1,105 @@
+function Stopwatch(elem) {
+    var time = 0;
+    var offset;
+    var interval;
+    function lapOn() {
+        var lapTime = lapContainer.appendChild(document.createElement("li"))
+        lapTime.innerHTML = elem.textContent;
+        lapTime.classList = 'lapItem'
+    }
+    function lapOff() {
+        return;
+    }
+    function update() {
+        if (this.isOn) {
+            time += delta();
+        }
+        elem.textContent = timeFormatter(time);
+    }
+    function delta() {
+        var now = Date.now();
+        var timePassed = now - offset;
+
+        offset = now;
+
+        return timePassed;
+    }
+    function timeFormatter(time) {
+        time = new Date(time);
+
+        var minutes = time.getMinutes().toString();
+        var seconds = time.getSeconds().toString();
+        var milliseconds = time.getMilliseconds().toString();
+
+        if (minutes.length < 2) {
+            minutes = '0' + minutes;
+        }
+
+        if (seconds.length < 2) {
+            seconds = '0' + seconds;
+        }
+
+        while (milliseconds.length < 3) {
+            milliseconds = '0' + minutes;
+        }
+
+        var result = minutes + ':' + seconds ;
+
+        return result;
+    }
+    this.start = function () {
+        interval = setInterval(update.bind(this), 1);
+        offset = Date.now();
+        this.isOn = true;
+    };
+    this.stop = function () {
+        clearInterval(interval);
+        interval = null;
+        this.isOn = false;
+    };
+    this.reset = function () {
+        time = 0;
+        lapContainer.innerHTML = '';
+        interval = null;
+        this.isOn = false;
+        update();
+    };
+    this.lapOn = function () {
+        lapOn();
+    }
+    this.lapOff = function () {
+        lapOff();
+    }
+    this.isOn = false;
+}
+var timer = document.querySelector('.timer');
+var toggleBtn = document.querySelector('.start');
+var resetBtn = document.querySelector('.reset');
+var lapBtn = document.querySelector('.stop');
+var lapContainer = document.querySelector('.lapContainer')
+var watch = new Stopwatch(timer);
+function start() {
+    toggleBtn.textContent = 'Pause';
+    toggleBtn.classList.toggle("on");
+    watch.start();
+}
+function stop() {
+    toggleBtn.textContent = 'Start';
+    toggleBtn.classList.toggle("on")
+    watch.stop();
+}
+function stopWhenOn() {
+    toggleBtn.textContent = 'Start';
+    toggleBtn.classList.toggle("on")
+    watch.stop();
+    watch.reset();
+}
+toggleBtn.addEventListener('click', function () {
+    watch.isOn ? stop() : start();
+});
+resetBtn.addEventListener('click', function () {
+    watch.isOn ? stopWhenOn() : watch.reset();
+});
+lapBtn.addEventListener('click', function () {
+    watch.isOn ? lapOn() : lapOff();
+})
